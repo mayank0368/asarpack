317cb1031099189958ec3034a0bb6b0583d6341d
mayank0368 <mastermind0368@gmail.com>
2019-07-17 15:43:52 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+var path = require("path");
+
+
+const server = express()
+
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(path.join(__dirname,"..","..", 'app.db'));
+
+db.serialize(function() {
+  db.run("CREATE TABLE IF NOT EXISTS lorem (info TEXT)");
+ 
+  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  for (var i = 1; i < 11; i++) {
+      stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
+}); 
+server.get('/', function (req, res) {
+
+    var obj ={};
+
+    db.serialize(function() {
+        db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+            obj[row.id] = row.info
+        }, function(){
+            console.log(obj)
+            res.send(obj)
+        });
+
+    });
+
+})
+
+server.get('/index', function (req, res) {
+    res.sendFile('index.html', {root: __dirname })
+})
+
+ 
+server.listen(8080)
