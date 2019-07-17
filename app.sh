461f8bc75f6205f1a1f23d187b59128a640b713a
mayank0368 <mastermind0368@gmail.com>
2019-07-17 17:04:26 +0530

    shell file fix for OSX

--- app.sh
+++ app.sh
@@ -1,1 +1,19 @@
-npm install
+mkdir temp
+./node_modules/.bin/electron-rebuild -w sqlite3 -p
+cp -R ./src ./temp/
+cp ./package-lock.json ./temp
+cp ./package.json ./temp
+cp -R ./node_modules ./temp
+npm install -g asar
+asar pack temp app.asar
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    rm -rf node_modules/electron/dist/resources/default_app.asar
+    cp app.asar node_modules/electron/dist/resources
+    cp -R node_modules/electron/dist app
+    mv app/electron app/app
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    rm -rf node_modules/electron/dist/Electron.app/Contents/Resources/default_app.asar
+    cp app.asar node_modules/electron/dist/Electron.app/Contents/Resources
+    cp -R node_modules/electron/dist/Electron.app app.app
+fi
+
