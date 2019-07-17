8ed376e7d7bf4412e5fc65fd73656a2385191fb6
mayank <mastermind0368@gmail.com>
2019-07-17 17:56:07 +0530

    shell linux fix

--- app.sh
+++ app.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 mkdir temp
 ./node_modules/.bin/electron-rebuild -w sqlite3 -p
 cp -R ./src ./temp/
@@ -8,12 +9,14 @@ npm install -g asar
 asar pack temp app.asar
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     rm -rf node_modules/electron/dist/resources/default_app.asar
-    cp app.asar node_modules/electron/dist/resources
-    cp -R node_modules/electron/dist app
-    mv app/electron app/app
+    mv app.asar node_modules/electron/dist/resources
+    mv node_modules/electron/dist app
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     rm -rf node_modules/electron/dist/Electron.app/Contents/Resources/default_app.asar
     cp app.asar node_modules/electron/dist/Electron.app/Contents/Resources
     cp -R node_modules/electron/dist/Electron.app app.app
 fi
+
+rm -rf temp
+
 
